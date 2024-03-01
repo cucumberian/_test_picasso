6780552781f235964bc05252d7eac7002da38b48
marsello <smamarsello@gmail.com>
2024-03-01 12:40:00 +0300

    feat: docker-compose.yml; renamed dir task/ -> src/

--- src/task/settings.py
+++ src/task/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = "task.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_DB_NAME"),
+        "NAME": os.environ.get("POSTGRES_DB"),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
         "HOST": os.environ.get("POSTGRES_HOST"),
